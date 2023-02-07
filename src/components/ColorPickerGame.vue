c03a5fbb87e006b9199d697e00b7fdf64bfc769b
Michael Ashibuogwu <damimyke@gmail.com>
2023-02-07 19:56:33 +0100

    Update ColorPickerGame.vue

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -8,29 +8,36 @@
   </button>
 </template>
 
+
+
+
+
 <script>
-import { ref, reactive } from "@vue/reactivity";
+import ColorPickerLogic from "../composables/ColorPickerLogic";
+export default { 
 
-export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
+   setup() { 
 
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+ const { colors, message, matchColor } = ColorPickerLogic(); 
 
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
+   return { 
 
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
+       colors, 
 
-    return { colors, message, matchColor };
-  },
-};
+       message, 
+
+       matchColor, 
+
+     }; 
+
+   }, 
+
+ };
+
+
+
 </script>
+  
+
 
 <style></style>
